c8a35f3773000d562664a38eca52466750eee7c6
Peter <pcborenstein@gmail.com>
2018-09-12 14:35:18 -0400

    added all base64 characters

--- set1/myConversions.c
+++ set1/myConversions.c
@@ -7,7 +7,7 @@
 
 const char input[] = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 const char output[] = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-const char base64[64 + 1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const char base64[64 + 1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 void testPrint(void){
     printf("hello world\n");
